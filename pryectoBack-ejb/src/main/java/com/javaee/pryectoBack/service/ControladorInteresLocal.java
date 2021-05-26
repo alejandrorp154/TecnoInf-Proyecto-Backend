35620f454d2904609931183065dc3cae387386f0
lucia <lucia.mele@fing.edu.uy>
2021-05-25 21:29:07 -0300

    alta, baja, modificar, getById, getAll intereses

--- pryectoBack-ejb/src/main/java/com/javaee/pryectoBack/service/ControladorInteresLocal.java
+++ pryectoBack-ejb/src/main/java/com/javaee/pryectoBack/service/ControladorInteresLocal.java
@@ -1,14 +1,18 @@
 package com.javaee.pryectoBack.service;
 
+import java.util.List;
+
 import javax.ejb.Local;
 
 import com.javaee.pryectoBack.datatypes.DTOInteres;
 
 @Local
 public interface ControladorInteresLocal {
-	boolean alta(DTOInteres dtoInteres);
-	boolean baja(DTOInteres dtoInteres);
-	boolean modificar(DTOInteres dtoInteres);
+	DTOInteres alta(DTOInteres dtoInteres);
+	boolean baja(int idInteres);
+	DTOInteres modificar(DTOInteres dtoInteres);
 	boolean suscribe(String idPersona, int idInteres);
 	boolean desuscribe(String idPersona, int idInteres);
+	DTOInteres getById(int idInteres);
+	List<DTOInteres> getAll(int offset, int size);
 }
