187636670b048c18e5d2b28e4bb299af90fff9de
tomgacami <tomgacami@gmail.com>
2021-06-07 11:46:26 -0300

    Eliminar cuenta Usuario Comun

--- pryectoBack-ejb/src/main/java/com/javaee/pryectoBack/data/ControladorUsuarioDA.java
+++ pryectoBack-ejb/src/main/java/com/javaee/pryectoBack/data/ControladorUsuarioDA.java
@@ -8,8 +8,14 @@ import com.javaee.pryectoBack.datatypes.DTOMultimedia;
 import com.javaee.pryectoBack.datatypes.DTOUsuario;
 import com.javaee.pryectoBack.datatypes.DTOUsuarioInicioSesion;
 import com.javaee.pryectoBack.model.*;
+import com.javaee.pryectoBack.util.MongoDBConnector;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
 
 @Singleton
 public class ControladorUsuarioDA implements ControladorUsuarioDALocal, ControladorUsuarioDARemote {
@@ -64,7 +70,7 @@ public class ControladorUsuarioDA implements ControladorUsuarioDALocal, Controla
 			Usuario user = manager.find(Usuario.class,idPersona);
 
 			if (user != null) {
-				
+
 				//Medallas
 				List<Medalla> medallas = user.getMedallas();
 				for (Medalla med : medallas) {
@@ -86,31 +92,85 @@ public class ControladorUsuarioDA implements ControladorUsuarioDALocal, Controla
 					manager.remove(ubic);
 				}
 
-				//Evento
-					//Creados por el usuario
+				//Eventos
+				//Creados por el usuario
 				List<Evento> eventosCreadosPorUsuario = user.getCreadorDeEventos();
-				for (Evento event : eventosCreadosPorUsuario){
+				if (!eventosCreadosPorUsuario.isEmpty()){
+					for (Evento event : eventosCreadosPorUsuario) {
 
-					List<Usuario> usuariosQueAsistiran = event.getUsuarios();
+						//Dessasigna a Usuarios que asistiran
+						List<Usuario> usuariosQueAsistiran = event.getUsuarios();
+						if (!usuariosQueAsistiran.isEmpty()) {
+							for (Usuario usuarioAsisteEvento : usuariosQueAsistiran) {
+								usuarioAsisteEvento.getEventos().remove(event);
+							}
+						}
 
-					for (Usuario usuarioAsisteEvento: usuariosQueAsistiran){
-						usuarioAsisteEvento.getEventos().remove(event);
+						//Desasigno las publicaciones del evento borrando sus comentarios
+						List<Publicacion> publicaciones = event.getPublicaciones();
+						if (!publicaciones.isEmpty()) {
+							for (Publicacion pub : publicaciones) {
+								MongoDBConnector mongoConnector = new MongoDBConnector();
+								MongoCollection<Document> collection = mongoConnector.getCollection("ComentariosYReacciones");
+
+								String idPublicacion = String.valueOf(pub.getIdPublicacion());
+								collection.deleteOne(eq("idPublicacion", new ObjectId(idPublicacion)));
+
+								pub.getPerfil().getPublicaciones().remove(pub);
+
+								manager.remove(pub);
+							}
+						}
+						manager.remove(event);
 					}
-
 				}
 
-					//Creados a los que asistira el usuario
+				//Eventos a los que asistira el usuario
+				List<Evento> eventos = user.getEventos();
+
+				if (!eventos.isEmpty()){
+					for (Evento event : eventos){
+						event.getUsuarios().remove(user);
+					}
+				}
+
+				//Remove Multimedia
+				PerfilUsuario userProfile = user.getPerfil();
+				List<Multimedia> galerias = userProfile.getGalerias();
+				if (!galerias.isEmpty()){
+					for (Multimedia multi : galerias){
+						manager.remove(multi);
+					}
+				}
+
+				//Publicaciones del Usuario
+				List<Publicacion> pubs = userProfile.getPublicaciones();
+				if (!pubs.isEmpty()){
+					for (Publicacion publicacion : pubs){
+
+						MongoDBConnector mongoConnector = new MongoDBConnector();
+						MongoCollection<Document> collection = mongoConnector.getCollection("ComentariosYReacciones");
+
+						String idPublicacion = String.valueOf(publicacion.getIdPublicacion());
+						collection.deleteOne(eq("idPublicacion", new ObjectId(idPublicacion)));
+
+						manager.remove(publicacion);
+					}
+				}
+
+				//Intereses del usuario
+				List<Interes> intereses = userProfile.getIntereses();
+				if (!intereses.isEmpty()){
+					for (Interes inter : intereses){
+						inter.getPerfiles().remove(userProfile);
+					}
+				}
 
 				//Perfil
-					//Publicaciones
-					//ComentariosYReacciones
-					//Intereses
-					//Galeria
+				manager.remove(userProfile);
 
-
-
-
-
+				//Remueve Usuario
+				manager.remove(user);
 
 				return true;
 			}
