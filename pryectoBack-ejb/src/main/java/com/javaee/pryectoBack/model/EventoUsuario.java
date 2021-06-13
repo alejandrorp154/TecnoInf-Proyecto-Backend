78599451904b2486b4676b1ec68a04d68129e367
lucia <lucia.mele@fing.edu.uy>
2021-06-12 22:10:37 -0300

    cambios para poder implementar invitar usuario a evento

---
+++ pryectoBack-ejb/src/main/java/com/javaee/pryectoBack/model/EventoUsuario.java
@@ -0,0 +1,57 @@
+package com.javaee.pryectoBack.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+@Entity
+@IdClass(EventoUsuarioId.class)
+public class EventoUsuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	private String idPersona;
+	@Id
+	private int idEvento;
+
+	@Column(length=32, columnDefinition = "varchar(32) default 'pendiente'")
+	@Enumerated(value = EnumType.STRING)
+	private estadosContactos estadoContactos;
+
+	public EventoUsuario() {
+	}
+
+	public EventoUsuario(String idPersona, int idEvento, estadosContactos estado) {
+		this.idPersona = idPersona;
+		this.idEvento = idEvento;
+		this.estadoContactos = estado;
+	}
+
+	public String getIdPersona() {
+		return idPersona;
+	}
+
+	public void setIdPersona(String idPersona) {
+		this.idPersona = idPersona;
+	}
+
+	public int getIdEvento() {
+		return idEvento;
+	}
+
+	public void setIdEvento(int idEvento) {
+		this.idEvento = idEvento;
+	}
+
+	public estadosContactos getEstadoContactos() {
+		return estadoContactos;
+	}
+
+	public void setEstadoContactos(estadosContactos estadoContactos) {
+		this.estadoContactos = estadoContactos;
+	}
+}
